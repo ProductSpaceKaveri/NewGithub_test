7a79bb93ab53fae5ca54d1b1119c3c5f8fada73e
ProductSpaceKaveri <kaveri@productspace.com>
2025-01-27 11:55:46 +0530

    Initial commit

---
+++ deployment/native/styles.js
@@ -0,0 +1,10 @@
+import * as style0 from "D:/NewGithub/themesource/administration/native/main";
+import * as style1 from "D:/NewGithub/themesource/feedbackmodule/native/main";
+import * as style2 from "D:/NewGithub/themesource/nanoflowcommons/native/main";
+import * as style3 from "D:/NewGithub/themesource/atlas_core/native/main";
+import * as style4 from "D:/NewGithub/themesource/myfirstmodule/native/main";
+import * as style5 from "D:/NewGithub/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5]);
